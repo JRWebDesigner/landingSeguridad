2baac5a53a5c0fcfb21b58784810bfc067c7d6e3
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-06-27 18:27:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+ 
 export const metadata: Metadata = {
   title: '¿ESTÁS SEGURO DE QUE TUS GUARDIAS ESTÁN CUMPLIENDO SU TRABAJO? ¿O SIMPLEMENTE LO SUPONES?',
   description: 'El sistema inteligente OnPatrol, te permite monitorear en tiempo real, auditar recorridos, registrar accesos y responder con evidencia a cualquier incidente, todo desde tu celular o PC.'
