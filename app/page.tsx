8842e3c0f4dafce59f6b4cd3555204632c19b512
Josue <jowebdesigc@gmail.com>
2025-06-27 22:27:58 -0400

    fin casi

--- app/page.tsx
+++ app/page.tsx
@@ -471,6 +471,10 @@ export default function SecurityMonitoringLanding() {
               </Card>
         </div>
       </section>
+      <footer className='relative z-10 flex justify-center items-center flex-col my-8 gap-6'>
+         <img src="./logob.png" width='270px' />
+         <span>Copyright © 2025 CORPORACIONROJO, Todos los derechos reservados</span>
+      </footer>
     </div>
   )
 }
